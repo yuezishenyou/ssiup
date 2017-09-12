32df4073e79455a22471192bb518462c7ef6eb5d
maoziyue <maoziyue@appledeMacBook-Pro.local>
2017-09-12 16:16:32 +0800

    first

---
+++ Pods/Target Support Files/Pods-ssiuo/Pods-ssiuo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ssiuo : NSObject
+@end
+@implementation PodsDummy_Pods_ssiuo
+@end
---
+++ ssiuo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ssiuo
+//
+//  Created by maoziyue on 2017/9/12.
+//  Copyright © 2017年 maoziyue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
